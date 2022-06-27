8c599b44f9c93945b3baa4702431b433cac6f706
Dhruba1010 <dhrubasinha096p@gmail.com>
2022-06-28 01:01:13 +0530

    completed both s1d3 and s2d1

---
+++ s1d3/src/Components/Login.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getAuthToken } from '../Redux/Auth/action';
+
+const Login = () => {
+    const [email, setEmail] = useState('')
+    const [pass, setPass] = useState('')
+    const dispatch = useDispatch();
+    const isAuth = useSelector(store => store.authReducer.isAuth)
+
+    const loginHandler = () => {
+        const payload = {email: email, password: pass}
+        dispatch(getAuthToken(payload))
+    }
+
+
+  return (
+    <>
+      {isAuth ? (<h3>Succesfully Logged In</h3>) : (
+        <div>
+            <input type="email" placeholder='Emaill' value={email} onChange={e => setEmail(e.target.value)} />
+            <input type="password" placeholder='Password' value={pass} onChange={e => setPass(e.target.value)} />
+            <button onClick={loginHandler}>Login</button>
+        </div>
+      )}
+    </>
+  )
+}
+
+export default Login
